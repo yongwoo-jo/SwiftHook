a671076017e4ced17442213a5e38b2d410d3e5eb
yongwoo <yongwoo.jo@kurlycorp.com>
2024-10-10 16:01:57 +0900

    Update Package.swift
    fix

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftHook",
+    platforms: [.iOS(.v12)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -13,7 +14,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/623637646/libffi.git", from: "3.4.5")
+        .package(url: "https://github.com/623637646/libffi.git", branch: "master")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
